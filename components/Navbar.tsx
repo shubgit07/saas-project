4450f729b7451cc9e7d87a78012ee54e4678ef8d
Shubham Verma <sh07verma@gmail.com>
2025-07-15 13:30:47 +0530

    Add Home page with companion sessions with routes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,16 +2,17 @@ import Image from "next/image"
 import Link from "next/link"
 import Navitems from "./Navitems"
 
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <Link href="/">
                 <div className="flex items-center gap-2.5 cursor-pointer">
                     <Image
-                        src="/images/apple-touch-icon.png."
+                        src="/images/logo.svg"
                         alt="logo"
                         width={46}
-                        height={44}
+                        height={46}
                     />
                 </div>
             </Link>
