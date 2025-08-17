25d7264729de6c75def75cdc682c2a0405a907da
Shubham Verma <sh07verma@gmail.com>
2025-08-17 19:28:00 +0530

    new 2

--- middleware.js
+++ middleware.js
@@ -1,16 +1,5 @@
 // Converted from middleware.ts to JavaScript
 
-const { clerkMiddleware } = require('@clerk/nextjs/server');
-
-const middleware = clerkMiddleware();
-
-module.exports = middleware;
-
-module.exports.config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+module.exports = (req, ev) => {
+  return new Response('OK');
 };
