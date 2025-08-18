42c4e523b25873147a5f32d19f9f494ccb153935
Shubham Verma <sh07verma@gmail.com>
2025-08-18 21:34:34 +0530

    deploy analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { Bricolage_Grotesque } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import ConditionalNavbar from "@/components/ConditionalNavbar";
+import { Analytics } from '@vercel/analytics/next';
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -12,15 +13,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
-        <ClerkProvider 
-          appearance={{ 
-            variables: { 
-              colorPrimary: '#fe5933' 
-            } 
+        <ClerkProvider
+          appearance={{
+            variables: {
+              colorPrimary: '#fe5933'
+            }
           }}
         >
           <ConditionalNavbar />
           {children}
+          <Analytics />
         </ClerkProvider>
       </body>
     </html>
